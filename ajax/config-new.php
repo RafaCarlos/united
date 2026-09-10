5b239db2800ecd5212454938379c80e6e93ed502
RafaCarlos <rcs.rafael@gmail.com>
2026-09-10 16:01:54 -0300

    Update config-new.php

--- ajax/config-new.php
+++ ajax/config-new.php
@@ -1,15 +1,3 @@
 <?php
 
 //DB details
-$dbHost = 'localhost';
-$dbUsername = 'unite987_usr5470';
-$dbPassword = '11@Un!ted';
-$dbName = 'unite987_unitedsite';
-
-//Create connection and select DB
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-$db->set_charset("utf8");
-
-if($db->connect_error){
-    die("Unable to connect database: " . $db->connect_error);
-}
